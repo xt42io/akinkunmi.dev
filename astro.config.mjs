dd14e9fa95e7c3f90bd414332921ec7475520149
Akinkunmi <akinkunmioye42@gmail.com>
2026-08-19 09:17:13 +0100

    Remove Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), react(), auth()],
   server:{
     allowedHosts:['.outray.app']
