ba095fbe2c45626985677bc14ce856660f73ed57
Akinkunmi <akinkunmioye42@gmail.com>
2026-08-19 09:25:31 +0100

    Configure standalone Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: node({ mode: "standalone" }),
   integrations: [tailwind(), react(), auth()],
   server:{
     allowedHosts:['.outray.app']
